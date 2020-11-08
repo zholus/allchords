2059ddb1f03c7076416c0d9e127ab1ad74a0c1d3
Alex <alzholus@gmail.com>
2020-11-08 15:25:30 +0100

    Move migrations to single folder

--- migrations/Version20201004114932.php
+++ migrations/Version20201004114932.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Modules\Comments\Infrastructure\Persistence\Migrations;
+namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
